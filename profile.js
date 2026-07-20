8fd92e4399bfde8b0b4d2dc83c7262b7ee954939
Ayush <adp5219@gmail.com>
2026-07-20 17:24:56 -0400

    Update profile.js

--- profile.js
+++ profile.js
@@ -11,7 +11,7 @@ If you do not have a link yet, leave that section as an empty list: []
 */
 
 const studentProfile = {
-  name: "Ayush Patel",
+  name: "Ayush D Patel",
 
   tagline: "DREAM-High Scholar | Computational Biology Portfolio",
 
